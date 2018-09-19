afe464b59cbd69d6111e0fbb814a37c47b088b49
DavisVaughan <davis@rstudio.com>
2018-09-19 10:15:34 -0400

    Document class_pred functions

--- R/class-pred.R
+++ R/class-pred.R
@@ -17,6 +17,33 @@ new_class_pred <- function(x, labels, ..., subclass = NULL) {
 
 }
 
+#' Create a class prediction object
+#'
+#' `class_pred()` creates a `class_pred` object from a factor or ordered
+#' factor. You can optionally specify values of the factor to be set
+#' as _equivocal_.
+#'
+#' Equivocal values are those that you feel unsure about, and would like to
+#' exclude from performance calculations or other metrics.
+#'
+#' By default, `probably` prints equivocal values as `EQ`. You can change this
+#' with the global option, `probably.equivocal_label`.
+#'
+#' @param x A factor or ordered factor.
+#' @param which An integer vector specifying the locations of `x` to declare
+#' as equivocal.
+#'
+#' @examples
+#'
+#' x <- factor(c("Yes", "No", "Yes", "Yes"))
+#'
+#' # Create a class_pred object from a factor
+#' class_pred(x)
+#'
+#' # Say you aren't sure about that 2nd "Yes" value. You could mark it as
+#' # equivocal.
+#' class_pred(x, which = 3)
+#'
 #' @export
 #' @importFrom rlang abort
 #' @importFrom vctrs vec_cast
@@ -105,12 +132,25 @@ format_as_factor <- function(x, ...) {
 # ------------------------------------------------------------------------------
 # Coercion
 
+#' Coerce to a `class_pred` object
+#'
+#' `as_class_pred()` provides coercion to `class_pred` from other
+#' existing objects.
+#'
+#' @inheritParams class_pred
+#'
+#' @examples
+#'
+#' x <- factor(c("Yes", "No", "Yes", "Yes"))
+#' as_class_pred(x)
+#'
 #' @export
-as_class_pred <- function(x, which) {
+as_class_pred <- function(x, which = integer()) {
   UseMethod("as_class_pred")
 }
 
-as_class_pred.default <- function(x, which) {
+#' @export
+as_class_pred.default <- function(x, which = integer()) {
   abort_default(x, "as_class_pred")
 }
 
@@ -125,6 +165,38 @@ as_class_pred.factor <- function(x, which = integer()) {
 # -----------------------
 # is_equivocal
 
+#' Locate equivocal values
+#'
+#' These functions provide multiple methods of checking for equivocal values,
+#' and finding their locations.
+#'
+#' @param x A `class_pred` object.
+#'
+#' @return
+#'
+#' `is_equivocal()` returns a logical vector the same length as `x`
+#' where `TRUE` means the value is equivocal.
+#'
+#' `which_equivocal()` returns an integer vector specifying the locations
+#' of the equivocal values.
+#'
+#' `any_equivocal()` returns `TRUE` if there are any equivocal values.
+#'
+#' @examples
+#'
+#' x <- class_pred(factor(1:10), which = c(2, 5))
+#'
+#' is_equivocal(x)
+#'
+#' which_equivocal(x)
+#'
+#' any_equivocal(x)
+#'
+#' @name locate-equivocal
+#'
+NULL
+
+#' @rdname locate-equivocal
 #' @export
 is_equivocal <- function(x) {
   UseMethod("is_equivocal")
@@ -148,6 +220,7 @@ is_equivocal.class_pred <- function(x) {
 # -----------------------
 # which_equivocal
 
+#' @rdname locate-equivocal
 #' @export
 which_equivocal <- function(x) {
   UseMethod("which_equivocal")
@@ -167,6 +240,7 @@ which_equivocal.class_pred <- function(x) {
 # -----------------------
 # any_equivocal
 
+#' @rdname locate-equivocal
 #' @export
 any_equivocal <- function(x) {
   UseMethod("any_equivocal")
@@ -185,6 +259,18 @@ any_equivocal.class_pred <- function(x) {
 # -----------------------
 # is_class_pred
 
+#' Test if an object inherits from `class_pred`
+#'
+#' `is_class_pred()` checks if an object is a `class_pred` object.
+#'
+#' @inheritParams is_equivocal
+#'
+#' @examples
+#'
+#' x <- class_pred(factor(1:5))
+#'
+#' is_class_pred(x)
+#'
 #' @export
 is_class_pred <- function(x) {
   inherits(x, "class_pred")
@@ -193,6 +279,20 @@ is_class_pred <- function(x) {
 # ------------------------------------------------------------------------------
 # Base S3 Methods
 
+#' Extract `class_pred` levels
+#'
+#' The levels of a `class_pred` object do _not_ include the equivocal value.
+#'
+#' @inheritParams is_equivocal
+#'
+#' @examples
+#'
+#' x <- class_pred(factor(1:5), which = 1)
+#'
+#' # notice that even though `1` is not in the `class_pred` vector, the
+#' # level remains from the original factor
+#' levels(x)
+#'
 #' @export
 levels.class_pred <- function(x) {
   attr(x, "labels")
