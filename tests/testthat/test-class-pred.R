a0b5b360b30f7fd83e7a25a73c4edc5308b731bc
DavisVaughan <davis@rstudio.com>
2018-09-24 09:14:36 -0400

    Add tests for coercion and retaining levels

--- tests/testthat/test-class-pred.R
+++ tests/testthat/test-class-pred.R
@@ -1,14 +1,21 @@
 context("test-class-pred")
 
+# ------------------------------------------------------------------------------
+# Setup
+
 ex_eq    <- c(0L, 1L, 2L, 2L, 3L)
 ex_no_eq <- c(1L, 1L, 2L, 2L, 3L)
 real_labs <- c("low", "med", "high")
 
 factor_no_eq <- factor(ex_no_eq, labels = real_labs)
 
-manual_creation_eq <- structure(ex_eq, labels = real_labs, class = c("class_pred", "vctrs_vctr"))
-manual_creation_no_eq <- structure(ex_no_eq, labels = real_labs, class = c("class_pred", "vctrs_vctr"))
+manual_creation_eq <- new_class_pred(ex_eq, labels = real_labs)
+manual_creation_no_eq <- new_class_pred(ex_no_eq, labels = real_labs)
 
+masked_high <- class_pred(factor_no_eq, which = 5)
+
+# ------------------------------------------------------------------------------
+# Testing
 
 test_that("can create from integers", {
   expect_equal(new_class_pred(ex_eq, real_labs), manual_creation_eq)
@@ -47,3 +54,20 @@ test_that("levels() does not return the equivocal label", {
   expect_equal(levels(manual_creation_eq), c("low", "med", "high"))
 })
 
+test_that("class_pred masking a level retains the level", {
+  expect_equal(
+    levels(masked_high),
+    c("low", "med", "high")
+  )
+})
+
+test_that("coercing class_pred to factor retains all levels", {
+  expect_equal(
+    levels(masked_high),
+    levels(as.factor(masked_high))
+  )
+})
+
+test_that("class_pred can be coerced to ordered factor", {
+  expect_is(as.ordered(manual_creation_eq), "ordered")
+})
