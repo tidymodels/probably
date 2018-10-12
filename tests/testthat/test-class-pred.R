8eee674057edd401e04cf392d6d0f3faa328ff42
DavisVaughan <davis@rstudio.com>
2018-10-12 15:08:53 -0400

    Add numerous tests regarding casting / coercion

--- tests/testthat/test-class-pred.R
+++ tests/testthat/test-class-pred.R
@@ -112,3 +112,129 @@ test_that("casting class_pred to factor", {
   expect_equal(vec_cast(cp1, fc3), ordered(c("a", "b", "b", "c")))
 
 })
+
+test_that("casting character to class_pred", {
+
+  chr1 <- c("a", "b", "b", "c")
+  cp1  <- class_pred(factor(c("a", "b", "b", "c")))
+  cp2  <- class_pred(factor(c("a", "b", "b", "b")))
+  cp3  <- class_pred(factor(c("a", "b", "b", "c"), ordered = TRUE))
+  cp4  <- class_pred(factor(c("a", "b", "b", "c")), equivocal = "eq")
+
+  # lossy cast, no c level in chr1
+  expect_equal(
+    expect_warning(
+      vec_data(vec_cast(chr1, cp2))
+    ),
+    c(1, 2, 2, NA)
+  )
+
+  # equivocal label is maintained
+  expect_equal(get_equivocal_label(vec_cast(chr1, cp4)), "eq")
+
+  # clean conversion to class_pred
+  expect_equal(vec_cast(chr1, cp1), class_pred(factor(c("a", "b", "b", "c"))))
+
+  # converting to ordered class_pred maintains orderedness
+  expect_true(is_ordered_class_pred(vec_cast(chr1, cp3)))
+
+})
+
+test_that("slicing", {
+
+  # levels are kept
+  expect_equal(levels(manual_creation_eq[1]), real_labs)
+
+  # reportable rate updates
+  expect_equal(reportable_rate(manual_creation_eq[1]), 0)
+  expect_equal(reportable_rate(manual_creation_eq[1:2]), 0.5)
+
+  # extending past adds NAs
+  expect_equal(vec_data(manual_creation_eq[1:6]), c(0, 1, 2, 2, 3, NA))
+
+})
+
+test_that("unknown casts are handled correctly", {
+
+  # numeric -> class_pred = error
+  expect_error(vec_cast(numeric(), class_pred()))
+
+  # logical vec -> class pred = depends on if only NA or has TRUE/FALSE
+  expect_equal(vec_data(vec_cast(NA, class_pred())), NA_real_)
+  expect_error(vec_data(vec_cast(TRUE, class_pred())))
+
+  # NULL second = x, NULL first = NULL
+  expect_equal(vec_cast(NULL, class_pred()), NULL)
+  expect_equal(vec_cast(class_pred(), NULL), class_pred())
+
+})
+
+test_that("combining class preds", {
+
+  cp1 <- new_class_pred(c(1L, 2L, 2L), c("low", "med"))
+  cp2 <- new_class_pred(c(1L, 2L, 2L), c("low", "med"), ordered = TRUE)
+  cp3 <- new_class_pred(c(1L, 2L, 2L), c("low", "med"), equivocal = "eq")
+
+  # joining with different levels is the union
+  expect_equal(levels(c(manual_creation_eq, cp1)), c("low", "med", "high"))
+
+  # joining with different levels is the union in the order given
+  # even if one is ordered
+  expect_equal(levels(c(manual_creation_eq, cp2)), c("low", "med", "high"))
+  expect_true(is_ordered_class_pred(c(manual_creation_eq, cp2)))
+
+  # joining with different equivocal labels uses the LHS label
+  expect_equal(get_equivocal_label(c(cp1, cp3)), "[EQ]")
+  expect_equal(get_equivocal_label(c(cp3, cp1)), "eq")
+
+})
+
+test_that("common type: factor and class_pred", {
+
+  expect_is(vec_type2(class_pred(), factor()), "class_pred")
+
+  expect_equal(
+    levels(
+      vec_type2(
+        class_pred(factor(levels = "a")),
+        factor(levels = "b")
+      )
+    ),
+    c("a", "b")
+  )
+
+  expect_equal(
+    get_equivocal_label(
+      vec_type2(
+        class_pred(factor(levels = "a"), equivocal = "eq"),
+        factor(levels = "b")
+      )
+    ),
+    "eq"
+  )
+
+  # reverse order
+
+  expect_is(vec_type2(factor(), class_pred()), "class_pred")
+
+  expect_equal(
+    levels(
+      vec_type2(
+        factor(levels = "b"),
+        class_pred(factor(levels = "a"))
+      )
+    ),
+    c("b", "a")
+  )
+
+  expect_equal(
+    get_equivocal_label(
+      vec_type2(
+        factor(levels = "b"),
+        class_pred(factor(levels = "a"), equivocal = "eq")
+      )
+    ),
+    "eq"
+  )
+
+})
