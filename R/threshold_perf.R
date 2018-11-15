b1dee3eebc386a5a01b9e6ec82be40b66e1b1d1e
DavisVaughan <davis@rstudio.com>
2018-11-15 11:12:51 -0500

    Improve threshold_perf() documentation

--- R/threshold_perf.R
+++ R/threshold_perf.R
@@ -1,29 +1,78 @@
-#' Generate data for thresholding two-class data
+#' Generate performance metrics across probability thresholds
 #'
-#' `threshold_perf` can take a set of class probability predictions
-#'  and determine performance characteristics across different values
-#'  of the probability threshold and any existing groups.
+#' `threshold_perf()` can take a set of class probability predictions
+#' and determine performance characteristics across different values
+#' of the probability threshold and any existing groups.
 #'
-#' @param .data A tibble or data frame, potentially grouped.
+#' Note that that the global option `yardstick.event_first` will be
+#' used to determine which level is the event of interest. For more details,
+#' see the Relevant level section of [yardstick::sens()].
 #'
-#' @param truth A two-level factor of the true outcome labels.
-#'  Note that that the global option `yardstick.event_first` will be
-#'  used to determine which level is the event of interest.
+#' The currently calculated metrics are:
+#' - [yardstick::j_index()]
+#' - [yardstick::sens()]
+#' - [yardstick::spec()]
+#' - `distance = (1 - sens) ^ 2 + (1 - spec) ^ 2`
 #'
-#' @param estimate A numeric vector of class probabilities such
-#'  that larger values are more indicative of the event of interest.
+#' @param .data A tibble, potentially grouped.
+#'
+#' @param truth The column identifier for the true two-class results
+#' (that is a factor). This should be an unquoted column name.
+#'
+#' @param estimate The column identifier for the predicted class probabilities
+#' (that is a numeric). This should be an unquoted column name.
 #'
 #' @param ... Currently unused.
 #'
 #' @param na_rm A single logical: should missing data be removed?
 #'
 #' @param thresholds A numeric vector of values for the probability
-#'  threshold to call a sample an event. If unspecified, a series
-#'  of values between 0.5 and 1.0 are used. **Note**: if this
-#'  argument is used, it must be named.
+#' threshold. If unspecified, a series
+#' of values between 0.5 and 1.0 are used. **Note**: if this
+#' argument is used, it must be named.
 #'
-#' @return A tibble with columns for the performance
-#'  characteristics, the threshold, and existing groups.
+#' @return A tibble with columns: `.threshold`, `.estimator`, `.metric`,
+#' `.estimate` and any existing groups.
+#'
+#' @examples
+#' library(dplyr)
+#' data("segment_logistic")
+#'
+#' # Set the threshold to 0.6
+#' # > 0.6 = good
+#' # < 0.6 = poor
+#' threshold_perf(segment_logistic, Class, .pred_good, thresholds = 0.6)
+#'
+#' # Set the threshold to multiple values
+#' thresholds <- seq(0.5, 0.9, by = 0.1)
+#'
+#' segment_logistic %>%
+#'   threshold_perf(Class, .pred_good, thresholds) %>%
+#'   print(n = 24)
+#'
+#' # ---------------------------------------------------------------------------
+#'
+#' # It works with grouped data frames as well
+#' # Let's mock some resampled data
+#' resamples <- 5
+#'
+#' mock_resamples <- resamples %>%
+#'   replicate(
+#'     expr = sample_n(segment_logistic, 100, replace = TRUE),
+#'     simplify = FALSE
+#'   ) %>%
+#'   bind_rows(.id = "resample")
+#'
+#' resampled_threshold_perf <- mock_resamples %>%
+#'   group_by(resample) %>%
+#'   threshold_perf(Class, .pred_good, thresholds)
+#'
+#' resampled_threshold_perf
+#'
+#' # Average over the resamples
+#' resampled_threshold_perf %>%
+#'   group_by(.metric, .threshold) %>%
+#'   summarise(.estimate = mean(.estimate))
 #'
 #' @export
 threshold_perf <- function(.data, ...) {
