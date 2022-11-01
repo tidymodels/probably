66b62446e4bb8a10aa9cc325580c62c02e91a359
Edgar Ruiz <edgararuiz@gmail.com>
2022-11-01 15:20:36 -0500

    Adds initial adjustment merge function

--- R/calibrate.R
+++ R/calibrate.R
@@ -1,16 +1,42 @@
 
-cal_object <- function(estimates_table, additional_classes = NULL) {
+cal_add_adj <- function(.data, cal_object) {
+  estimates <- cal_get_estimates(cal_object)
+  col_names <- colnames(estimates)
+  match_col <- col_names[[1]]
+  round_data <- mutate(
+    .data,
+    .rounded = round(!! parse_expr(match_col), digits = 3)
+  )
+  matched_data <- dplyr::left_join(
+    round_data,
+    estimates,
+    by = c(".rounded" = match_col)
+  )
+  dplyr::select(matched_data, - .rounded)
+}
+
+cal_object <- function(estimates_table, truth, truth_val,
+                       additional_classes = NULL
+                       ) {
   structure(
     list(
+      truth = truth,
+      truth_val = truth_val,
       estimates_table = estimates_table
     ),
     class = c("cal_object", additional_classes)
   )
 }
 
+cal_get_estimates <- function(x) {
+  x$estimates_table
+}
+
 cal_glm_dataframe <- function(.data, truth, estimate, truth_val = 1) {
   truth <- enquo(truth)
   estimate <- enquo(estimate)
+
+  adj_name <- parse_expr(paste0(as_name(estimate), "_adj_glm"))
 
   val_data <- dplyr::mutate(
     .data,
@@ -20,15 +46,20 @@ cal_glm_dataframe <- function(.data, truth, estimate, truth_val = 1) {
 
   model <- glm(.is_val ~ .estimate, data = val_data, family = "binomial")
 
-  new_estimates <- seq_len(1000) * 0.001
+  new_estimates <- round(seq_len(1000) * 0.001, digits = 3)
   pred <- predict(model, newdata = data.frame(.estimate = new_estimates), type = "response")
 
   tbl_estimates <- tibble(
-    estimate = new_estimates,
-    adj_estimate = pred
+    !! estimate := new_estimates,
+    !! adj_name := pred
   )
 
-  cal_object(tbl_estimates, c("cal_glm", "cal_binary"))
+  cal_object(
+    estimates_table = tbl_estimates,
+    truth = truth,
+    truth_val = truth_val,
+    additional_classes = c("cal_glm", "cal_binary")
+    )
 }
 
 probability_bins <- function(.data, truth, estimate, truth_val = 1, no_bins = 10) {
