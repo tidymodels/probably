1f5eaa6963cbf18f78464b5eec442781ff3aa0ab
Edgar Ruiz <edgararuiz@gmail.com>
2022-11-08 09:21:04 -0600

    Adds documentation for plotting functions, makes proper imports to avoid errors in R checks

--- R/calibrate.R
+++ R/calibrate.R
@@ -1,10 +1,44 @@
+#' Calibration plots
+#' @description Calibration plot functions. They require a data.frame that contains
+#' the predictions and probability columns. The output is a `ggplot2` graph.
+#'
+#' @param .data A data.frame object containing predictions and probability columns
+#' @param truth The column identifier for the true class results
+#' (that is a factor). This should be an unquoted column name
+#' @param estimate The column identifier for the prediction probabilities.
+#' This should be an unquoted column name
+#' @param num_breaks The number of segments to group the probabilities. Defaults
+#' to 10
+#' @param conf_level Confidence level to use in the visualization. Applies to
+#' the ribbon layer. Defaults to 0.9
+#' @param include_ribbon Flag that indicates if the ribbon layer is to be
+#' included. Defaults to `TRUE`
+#' @param include_rug Flag that indicates if the Rug layer is to be included.
+#' Defaults to `TRUE`
+#' @examples
+#'
+#' library(ggplot2)
+#'
+#' cal_binary_plot_breaks(
+#'   segment_logistic,
+#'   Class,
+#'   .pred_good
+#' )
+#'
+#' cal_binary_plot_logistic(
+#'   segment_logistic,
+#'   Class,
+#'   .pred_good
+#' )
+#'
+#' @export
 cal_binary_plot_breaks <- function(.data,
                                    truth,
                                    estimate,
                                    num_breaks = 10,
                                    conf_level = 0.90,
-                                   include_rug = TRUE,
-                                   include_ribbon = TRUE
+                                   include_ribbon = TRUE,
+                                   include_rug = TRUE
                                    ) {
 
   truth <- enquo(truth)
@@ -16,7 +50,7 @@ cal_binary_plot_breaks <- function(.data,
 
   if(length(truth_levels) != 2) stop("'", truth_name, "' does not have 2 levels")
 
-  prob_tbl <- probability_breaks(
+  prob_tbl <- cal_probability_breaks(
     .data = .data,
     truth = !!truth,
     estimate = !!estimate,
@@ -26,14 +60,17 @@ cal_binary_plot_breaks <- function(.data,
 
   sub_title <- paste0("'", truth_name, "' is equal to '", truth_levels[[1]], "'")
 
-  res <- ggplot(data = prob_tbl, aes(x = predicted_midpoint)) +
-    geom_abline(col = "#999999", lty = 2) +
+  res <- ggplot(
+    data = prob_tbl,
+    aes(x = predicted_midpoint)
+    ) +
+    geom_abline(col = "#999999", linetype = 2) +
     geom_line(aes(y = event_rate)) +
     geom_point(aes(y = event_rate))
 
   if(include_ribbon) {
     res <- res +
-      geom_ribbon(aes(y = event_rate, ymin = lower, ymax = upper), alpha = 1 / 5)
+      geom_ribbon(aes(y = event_rate, ymin = lower, ymax = upper), alpha = 0.1)
   }
 
   if(include_rug) {
@@ -44,20 +81,20 @@ cal_binary_plot_breaks <- function(.data,
         data = level_1_tbl,
         aes(x = !!estimate, col = !!truth),
         sides = "b",
-        cex = 1 / 5,
+        cex = 0.2,
         show.legend = FALSE
         ) +
       geom_rug(
         data = level_2_tbl,
         aes(x = !!estimate, col = !!truth),
         sides = "t",
-        cex = 1 / 5,
+        cex = 0.2,
         show.legend = FALSE
         )
   }
 
   res +
-    coord_obs_pred() +
+    tune::coord_obs_pred() +
     labs(
       title = "Calibration Plot",
       subtitle = sub_title,
@@ -66,6 +103,8 @@ cal_binary_plot_breaks <- function(.data,
     )
 }
 
+#' @rdname cal_binary_plot_breaks
+#' @export
 cal_binary_plot_logistic <- function(.data,
                                      truth,
                                      estimate,
@@ -83,21 +122,25 @@ cal_binary_plot_logistic <- function(.data,
 
   if(length(truth_levels) != 2) stop("'", truth_name, "' does not have 2 levels")
 
-  prob_tbl <- probability_logistic(
+  prob_tbl <- cal_probability_logistic(
     .data = .data,
     truth = !!truth,
-    estimate = !!estimate
+    estimate = !!estimate,
+    conf_level = conf_level
   )
 
   sub_title <- paste0("'", truth_name, "' is equal to '", truth_levels[[1]], "'")
 
-  res <- ggplot(data = prob_tbl, aes(x = estimate)) +
+  res <- ggplot(
+    data = prob_tbl,
+    aes(x = estimate)
+    ) +
     geom_abline(col = "#999999", linetype = 2) +
     geom_line(aes(y = prob))
 
   if(include_ribbon) {
     res <- res +
-      geom_ribbon(aes(y = prob, ymin = lower, ymax = upper), alpha = 1 / 5)
+      geom_ribbon(aes(y = prob, ymin = lower, ymax = upper), alpha = 0.1)
   }
 
   if(include_rug) {
@@ -108,20 +151,20 @@ cal_binary_plot_logistic <- function(.data,
         data = level_1_tbl,
         aes(x = !!estimate, col = !!truth),
         sides = "b",
-        cex = 1 / 5,
+        cex = 0.2,
         show.legend = FALSE
       ) +
       geom_rug(
         data = level_2_tbl,
         aes(x = !!estimate, col = !!truth),
         sides = "t",
-        cex = 1 / 5,
+        cex = 0.2,
         show.legend = FALSE
       )
   }
 
   res +
-    coord_obs_pred() +
+    tune::coord_obs_pred() +
     labs(
       title = "Calibration Plot",
       subtitle = sub_title,
@@ -130,36 +173,41 @@ cal_binary_plot_logistic <- function(.data,
     )
 }
 
-probability_breaks <- function(.data,
-                               truth,
-                               estimate,
-                               num_breaks = 10,
-                               conf_level = 0.90
-                               ) {
+cal_probability_breaks <- function(.data,
+                                   truth,
+                                   estimate,
+                                   num_breaks = 10,
+                                   conf_level = 0.90
+                                   ) {
   truth <- enquo(truth)
   estimate <- enquo(estimate)
 
-  bin_exprs <- map(
+  bin_exprs <- purrr::map(
     seq_len(num_breaks),
     ~ expr(!!estimate <= !!.x / !!num_breaks ~ !!.x)
   )
 
   bin_data <- .data %>%
     dplyr::mutate(
-      .bin = case_when(!!!bin_exprs),
+      .bin = dplyr::case_when(!!!bin_exprs),
       .is_val = ifelse(as.integer(!!truth) == 1, 1, 0)
     ) %>%
     dplyr::group_by(.bin, .add = TRUE) %>%
     dplyr::summarise(
       predicted_midpoint = median(!!estimate),
-      event_rate = sum(.is_val) / n(),
+      event_rate = sum(.is_val) / dplyr::n(),
       events = sum(.is_val),
-      total = n()
+      total = dplyr::n()
     ) %>%
     dplyr::ungroup() %>%
     dplyr::select(-.bin)
 
-  add_conf_intervals(bin_data, events, total, conf_level = conf_level)
+  add_conf_intervals(
+    .data = bin_data,
+    events = events,
+    total = total,
+    conf_level = conf_level
+    )
 }
 
 add_conf_intervals <- function(.data,
@@ -178,7 +226,7 @@ add_conf_intervals <- function(.data,
         suppressWarnings(
           pt <- prop.test(events, total, conf.level = conf_level)
         )
-        ret <- as_tibble(.x)
+        ret <- tibble::as_tibble(.x)
         ret$lower <- pt$conf.int[[1]]
         ret$upper <- pt$conf.int[[2]]
         ret
@@ -186,7 +234,11 @@ add_conf_intervals <- function(.data,
     )
 }
 
-probability_logistic <- function(.data, truth, estimate) {
+cal_probability_logistic <- function(.data,
+                                     truth,
+                                     estimate,
+                                     conf_level = 0.90
+                                     ) {
   truth <- enquo(truth)
   estimate <- enquo(estimate)
 
@@ -197,21 +249,22 @@ probability_logistic <- function(.data, truth, estimate) {
     data = prep_data,
     family = binomial()
     )
+
   new_seq <- seq(0, 1, by = .01)
   new_data <- data.frame(estimate = new_seq)
   preds <- predict(model, new_data, type = "response", se.fit = TRUE)
 
-  res <- tibble(
+  res <- tibble::tibble(
     prob = 1 - preds$fit,
     se_fit = preds$se.fit
   )
 
   res <- cbind(new_data, res)
 
-  res$lower <- res$prob - qnorm(0.95) * res$se_fit
-  res$upper <- res$prob + qnorm(0.95) * res$se_fit
+  res$lower <- res$prob - qnorm(conf_level) * res$se_fit
+  res$upper <- res$prob + qnorm(conf_level) * res$se_fit
   res$se_fit <- NULL
 
-  as_tibble(res)
+  tibble::as_tibble(res)
 
 }
--- R/probably-package.R
+++ R/probably-package.R
@@ -4,6 +4,7 @@
 ## usethis namespace: start
 #' @import rlang
 #' @import vctrs
+#' @import ggplot2
 #' @importFrom magrittr %>%
 #' @importFrom yardstick sens spec j_index
 ## usethis namespace: end
