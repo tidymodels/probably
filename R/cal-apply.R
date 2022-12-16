fe49e27fd746541b105b8018efb8f9b564e25620
Edgar Ruiz <edgararuiz@gmail.com>
2022-12-16 14:59:59 -0600

    Adds support for new cal_object structure in cal_apply()

--- R/cal-apply.R
+++ R/cal-apply.R
@@ -70,11 +70,21 @@ cal_add_adjust.cal_estimate_isotonic <- function(object, .data) {
 
 cal_add_predict_impl <- function(object, .data) {
   if (object$type == "binary") {
-    model <- object$estimates
-    preds <- predict(model, newdata = .data, type = "response")
-    preds <- 1 - preds
-    .data[object$levels[[1]]] <- preds
-    .data[object$levels[[2]]] <- 1 - preds
+    .data <- object$estimates %>%
+      purrr::map(
+        ~ {
+          if(is.null(.x$filter)) {
+            new_data <- .data
+          } else {
+            new_data <- dplyr::filter(.data, !! .x$filter)
+          }
+          preds <- predict(.x$estimate, newdata = new_data, type = "response")
+          preds <- 1 - preds
+          new_data[object$levels[[1]]] <- preds
+          new_data[object$levels[[2]]] <- 1 - preds
+          new_data
+        }) %>%
+      purrr::reduce(dplyr::bind_rows)
   }
   .data
 }
