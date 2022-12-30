2147d49f83c34c7720fa033a9bc75cbf29f4f010
Edgar Ruiz <edgararuiz@gmail.com>
2022-12-30 17:53:40 -0600

    Adds summarization argument

--- R/cal-validate.R
+++ R/cal-validate.R
@@ -1,5 +1,8 @@
 #' Obtain and validate performance metrics from calibration
 #' @param metrics A set of metrics passed created via `yardstick::metric_set()`
+#' @param summarize Indicates to pass tibble with the metrics averaged, or
+#' if to return the same sampled object but with new columns containing the
+#' calibration y validation list columns.
 #' @inheritParams cal_estimate_logistic
 #' @export
 cal_validate_logistic <- function(.data,
@@ -8,6 +11,7 @@ cal_validate_logistic <- function(.data,
                                   smooth = TRUE,
                                   parameters = NULL,
                                   metrics = NULL,
+                                  summarize = TRUE,
                                   ...) {
   UseMethod("cal_validate_logistic")
 }
@@ -19,6 +23,7 @@ cal_validate_logistic.rset <- function(.data,
                                   smooth = TRUE,
                                   parameters = NULL,
                                   metrics = NULL,
+                                  summarize = TRUE,
                                   ...) {
   cal_validate(
     rset = .data,
@@ -26,6 +31,7 @@ cal_validate_logistic.rset <- function(.data,
     estimate = {{estimate}},
     cal_function = "logistic",
     metrics = metrics,
+    summarize = summarize,
     ...
   )
 }
@@ -37,6 +43,7 @@ cal_validate <- function(rset,
                          estimate = NULL,
                          cal_function = NULL,
                          metrics = NULL,
+                         summarize = TRUE,
                          ...
                          ) {
   if(is.null(cal_function)) rlang::abort("No calibration function provided")
@@ -78,17 +85,41 @@ cal_validate <- function(rset,
     }) %>%
     purrr::transpose()
 
-  dplyr::mutate(
+  x <- dplyr::mutate(
     rset,
     calibration = cals,
     validation = applied$val,
     stats_after = applied$stats_after,
     stats_before = applied$stats_before
   )
+
+  if(summarize) x <- summarize_validation(x)
+
+   x
 }
 
 #' @importFrom pillar type_sum
 #' @export
 type_sum.cal_binary <- function(x, ...) {
   paste0(x$method, " [", x$rows, "]")
+}
+
+
+summarize_validation <- function(x) {
+
+  fs <- x$stats_after[[1]]
+
+  seq_len(nrow(fs)) %>%
+    map(~ {
+      y <- .x
+      sa <- purrr::map_dbl(x$stats_after, ~ .x[y, ]$.estimate)
+      mean_sa <- mean(sa)
+      sb <- purrr::map_dbl(x$stats_before, ~ .x[y, ]$.estimate)
+      mean_sb <- mean(sb)
+      ret <- fs[y, ]
+      ret$.before <- mean_sb
+      ret$.after <- mean_sa
+      ret
+    }) %>%
+    dplyr::bind_rows()
 }
